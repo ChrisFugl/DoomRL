0c453f5d7f75d3d0bd3af79966f3e0e3b5f47b0a
haidertom <haidertomts@gmail.com>
2019-06-25 19:00:16 +0200

    added ppo2 baseline

---
+++ models/baseline_ppo2.py
@@ -0,0 +1,40 @@
+from baselines.ppo2.ppo2 import learn
+from baselines.common.tf_util import get_session
+import tensorflow as tf
+
+
+def run(config, env):
+    session_config = tf.ConfigProto(
+        allow_soft_placement=True,
+        intra_op_parallelism_threads=1,
+        inter_op_parallelism_threads=1
+    )
+    session_config.gpu_options.allow_growth = True
+    get_session(config=session_config)
+    model = learn(
+        env=env,
+        total_timesteps=config.timesteps,
+        network='cnn',
+        seed=config.seed,
+        ent_coef=config.entropy_weight, #default = 0
+        lr=config.learning_rate,
+        vf_coef=config.critic_weight, #default = 0.5
+        max_grad_norm=config.max_grad_norm,
+        gamma=config.discount_factor,
+        nsteps=config.number_of_steps,
+        eval_env = None,
+        lam=0.95,
+        log_interval=2,
+        nminibatches=4,
+        noptepochs=4,
+        cliprange=0.2,
+        save_interval=0,
+        load_path=None,
+        model_fn=None,
+        update_fn=None,
+        init_fn=None,
+        mpi_rank_weight=1,
+        comm=None
+        )
+
+    return model
